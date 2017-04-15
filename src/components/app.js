32f41f7c538e579992bfb089a7281e214196673b
Hack Reactor Student <student@hackreactor.com>
2017-04-15 16:19:47 -0700

    Pass through App tests

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,4 @@
 angular.module('video-player')
-// .controller('appCtrl', function($scope) {
-//   $scope.videoListData = window.exampleVideoData;
-//   $scope.currentVideo = {};
-//   
-// })
 .directive('app', function() {
   return {
     scope: {
@@ -12,20 +7,21 @@ angular.module('video-player')
     resrict: 'E',
     controllerAs: 'ctrl',
     bindToController: true,
-    controller: function() {
-      // var $injector = angular.injector();
-      // console.log($injector.get('youTube'));
-      this.currentVideo = {};
-      this.videos = window.exampleVideoData;
+    controller: function(youTube) {
       this.selectVideo = (video) => {
         console.log(this, this.currentVideo);
         this.currentVideo = video;
       };
-      this.searchResults = () => {
-
+      this.searchResults = (data) => {
+        this.videos = data;
+        this.currentVideo = data[0];
       };
+      
+      this.videos = window.exampleVideoData;
+      this.currentVideo = {};
 
-  
+      youTube.search('donald', this.searchResults);
+      
     },
     templateUrl: 'src/templates/app.html'
   };
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,11 +1,19 @@
 angular.module('video-player')
-.service('youTube', function() {
-  $http({
-  	method: 'GET',
-  	url: '',
-  }).then(function successCallback(respone) {
-  	console.log('Success!');
-  }, function errorCallback(response) {
-  	console.log('Failure!');
-  });
+.service('youTube', function($http) {
+  this.search = function(str, callback) {
+	  $http({
+	  	method: 'GET',
+	  	url: 'https://www.googleapis.com/youtube/v3/search',
+	  	params: {maxResults: 5, query: str, key: window.YOUTUBE_API_KEY, type: 'video', videoEmbeddable: true, part: 'snippet'}
+	  }).then(function successCallback(data) {
+	  	console.log('success');
+	  	callback(data.data.items);
+	  	console.log(data);
+	  }, function errorCallback(response) {
+	  	console.log('failure');
+	  	console.log(response);
+
+	  });
+  	
+  }
 });
